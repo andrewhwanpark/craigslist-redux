5f13b0f5f6244afb7ffb849164d6cb6b67de6ea3
Andrew Park <hp1141@stern.nyu.edu>
2020-06-06 12:18:09 -0400

    Added Bootstrap, Basic Navbar, Main, Sticky Footer

---
+++ src/components/Footers.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import { Container } from "react-bootstrap"
+
+function Footers() {
+    return (
+        <footer className="footer mt-auto py-3">
+            <Container>
+                <span className="text-muted">Place sticky footer content here.</span>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footers
---
+++ src/components/MainContent.js
@@ -0,0 +1,106 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae
+      tellus nec massa dictum pharetra at et erat. Nam eu feugiat nisi.
+      Vestibulum eu nisl sapien. Nam sodales vitae nisl nec varius. Nam eu ex
+      bibendum nibh viverra aliquet. Aenean lobortis nulla in tempor mollis.
+      Duis eu lacinia leo. Mauris sollicitudin sollicitudin metus, a dictum leo
+      ullamcorper vel. Maecenas nec malesuada quam, eget aliquam sem. Etiam
+      ullamcorper sapien elementum nibh convallis, suscipit euismod justo
+      suscipit. Aenean vestibulum vehicula arcu, lacinia lobortis est bibendum
+      quis. Suspendisse non justo ac arcu hendrerit scelerisque at vitae erat.
+      Sed mattis ex at dui tincidunt, ut tempor felis dapibus. Sed auctor
+      vulputate nibh, sed varius justo rhoncus id. Nullam iaculis maximus nibh
+      quis luctus. Duis ac viverra nisl, a mattis lectus. Phasellus placerat
+      ultricies nisl et dignissim. Duis sodales velit et risus suscipit rutrum.
+      Quisque venenatis mollis convallis. Quisque sed orci in urna tincidunt
+      ultricies. Morbi justo diam, tempus ac felis et, euismod aliquam augue.
+      Pellentesque in condimentum lorem, quis vulputate dolor. Mauris dapibus
+      tempus convallis. Cras sem magna, vestibulum non porttitor ac, tempus
+      vitae mauris. Etiam pulvinar auctor velit, a venenatis mauris. Donec
+      pellentesque enim vel euismod bibendum. Fusce tempor mi leo, nec tristique
+      dui imperdiet tempor. Ut erat sapien, interdum ut dui id, tempor pulvinar
+      eros. In in nulla feugiat, hendrerit erat porta, posuere urna. Etiam
+      porta, ante in mollis vestibulum, eros arcu iaculis eros, ut feugiat diam
+      lectus at augue. Quisque tristique est in lectus facilisis consequat.
+      Suspendisse hendrerit pretium lorem, vel molestie orci interdum id.
+      Pellentesque vel enim arcu. Praesent aliquam tincidunt vestibulum. Mauris
+      sed porta sem. Aliquam erat volutpat. Maecenas nunc eros, aliquet at elit
+      rutrum, viverra tincidunt sapien. In vulputate tincidunt eleifend. Aenean
+      mollis commodo mi, eu placerat metus scelerisque nec. Nam laoreet nisi
+      nibh, vel dapibus quam pellentesque sit amet. Donec vel molestie quam, in
+      pulvinar nulla. Morbi consequat dui nunc, non condimentum odio finibus in.
+      Cras lobortis quam ac erat mattis tempus. Duis dui lectus, venenatis ac
+      efficitur eget, auctor et ante. Sed luctus fermentum purus, nec tristique
+      nisl accumsan eu. Suspendisse potenti. Maecenas a ex ultrices, posuere
+      risus in, dapibus quam. Nulla aliquet lacus non odio bibendum porta. Proin
+      auctor placerat posuere. Aliquam sollicitudin, nunc ac varius interdum,
+      purus dolor condimentum mauris, ac tincidunt lorem arcu eu orci. Nullam
+      aliquam nulla sem, eget eleifend ligula sagittis et. Aliquam urna metus,
+      condimentum eget sapien eget, rutrum commodo diam. Cras commodo gravida
+      malesuada. Donec tempor euismod massa, sed sagittis lacus pharetra non.
+      Vestibulum ligula tellus, pharetra ac scelerisque vitae, porta vitae mi.
+      Fusce at tempus dolor. Donec condimentum turpis non velit gravida viverra.
+      Vestibulum tristique ut orci quis egestas. Cras quam risus, consectetur a
+      augue sed, congue blandit arcu. Sed tristique diam diam. Nam pulvinar
+      fermentum consequat. Nam tincidunt consequat eros gravida tincidunt.
+      Vestibulum at dolor ac risus mattis ullamcorper eget non ipsum. Donec ex
+      urna, malesuada in fringilla non, rhoncus non elit. Nam facilisis tellus
+      id mi fringilla, porttitor ullamcorper nisi bibendum. Nam arcu odio,
+      elementum nec sem sed, lacinia molestie tellus. Quisque id quam faucibus,
+      hendrerit risus at, facilisis risus. Etiam maximus condimentum felis nec
+      rhoncus. Vestibulum eros lacus, consectetur non sapien sit amet, tristique
+      luctus tortor. Aliquam tincidunt feugiat justo, non auctor nulla iaculis
+      id. Donec sed lacus sit amet ligula auctor luctus non at justo. Mauris non
+      tincidunt orci, ut pharetra sapien. Quisque porttitor sodales arcu,
+      dignissim venenatis leo rutrum vel. Cras eros augue, mattis a est ac,
+      rhoncus dignissim nulla. Quisque quis commodo turpis. Nam vel pulvinar
+      ipsum. Donec tempus lorem et ex laoreet, sit amet tristique massa
+      placerat. Phasellus eget lectus ut nisl luctus congue ut sed massa.
+      Integer eget urna vitae erat feugiat facilisis vitae sit amet turpis. Nunc
+      eros urna, finibus nec aliquam a, posuere sed tellus. Sed ipsum metus,
+      lobortis et faucibus id, suscipit id ligula. Nunc fringilla tincidunt
+      augue. Pellentesque nibh velit, congue ut laoreet non, vehicula vel enim.
+      Maecenas molestie elit in nisl aliquet gravida. Proin vitae rhoncus
+      lectus. Curabitur sodales, dui id luctus elementum, orci purus blandit
+      erat, ac varius elit purus a tortor. Pellentesque volutpat non lacus vitae
+      elementum. Nunc imperdiet sapien id lacus ultricies, at imperdiet ligula
+      finibus. Vestibulum convallis euismod massa sit amet mollis. Praesent
+      tempus sem ante, eget pellentesque massa aliquet eget. Nullam ac diam
+      auctor, dictum erat sit amet, ultricies ex. Maecenas eu lacus pulvinar,
+      laoreet libero nec, pulvinar enim. Sed congue egestas metus ac hendrerit.
+      Mauris feugiat sodales est, non vulputate ligula bibendum in. Etiam non
+      leo placerat, aliquam ante faucibus, egestas nisi. Vestibulum ante ipsum
+      primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed nec
+      sagittis sem, et ultrices erat. Etiam id dui suscipit, sodales ligula in,
+      elementum ante. Nullam eleifend, mi a tempor tempus, turpis nunc cursus
+      nibh, at vulputate risus lacus sed lacus. Proin vitae tincidunt sapien.
+      Vestibulum nulla nibh, pretium a nisl a, consequat finibus purus. Duis
+      tempor condimentum congue. Aenean aliquam, lectus sed auctor sagittis,
+      diam neque scelerisque ipsum, et accumsan orci ante ac dolor. Nulla vitae
+      ornare tellus, nec eleifend quam. Nulla varius leo nec dolor tincidunt
+      volutpat. Nunc vitae ligula dapibus, elementum justo sit amet, pulvinar
+      leo. In dui eros, lacinia non mi in, congue malesuada orci. Nunc efficitur
+      porttitor nisi, eget cursus est gravida non. Nunc efficitur sodales est et
+      dapibus. Vivamus vehicula, eros non porta facilisis, nunc nulla posuere
+      mauris, tincidunt dictum leo sem vel ex. Vestibulum malesuada, est id
+      aliquet bibendum, orci magna sodales sapien, ut viverra erat ante vitae
+      odio. Integer eleifend interdum magna, id laoreet justo interdum a. Nullam
+      eu finibus quam. Curabitur vitae placerat urna. Nam a commodo est. Donec
+      id pharetra turpis, non eleifend nulla. Nam elementum vehicula convallis.
+      Morbi sollicitudin ligula ac tincidunt interdum. In ligula lacus, maximus
+      tincidunt orci vitae, euismod euismod ex. Aenean maximus condimentum
+      venenatis. Nulla sed sapien ullamcorper purus viverra finibus a in turpis.
+      Nam et nisi nibh. Integer pellentesque pulvinar mollis. Nullam lacinia
+      magna ac condimentum ullamcorper. Quisque sed convallis dolor. Nulla non
+      gravida tortor, a accumsan augue. Donec egestas ante eu semper semper.
+      Etiam metus ipsum, lobortis eu ultricies ultricies, cursus tincidunt
+      libero.
+    </p>
+  );
+}
+
+export default MainContent;
