6cd273a4829496ae8a96275e63bc551bb64bcb8b
Andrew Park <hp1141@stern.nyu.edu>
2020-06-12 20:39:01 -0400

    Added handling for listings without images

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -3,19 +3,23 @@ import { Col, Card, Badge } from "react-bootstrap";
 import placeholder from "../placeholder.png";
 
 function Listing({ title, price, date, image, desc }) {
-  return (
-    <Col lg={3} md={6} xs={6} className="my-4">
-      <Card>
-        <Card.Img variant="top" src={image} />
-        <Card.Body>
-          <Card.Title>
-            {title} <Badge variant="secondary">${price}</Badge>
-          </Card.Title>
-          <Card.Text>{date}</Card.Text>
-        </Card.Body>
-      </Card>
-    </Col>
-  );
+    // If no image src, then replace with placeholder
+    let src;
+    image == "null" ? src = placeholder : src = image
+
+    return (
+        <Col lg={3} md={6} xs={6} className="my-4">
+        <Card>
+            <Card.Img variant="top" src={src} />
+            <Card.Body>
+            <Card.Title>
+                {title} <Badge variant="secondary">${price}</Badge>
+            </Card.Title>
+            <Card.Text>{date}</Card.Text>
+            </Card.Body>
+        </Card>
+        </Col>
+    );
 }
 
 export default Listing;
