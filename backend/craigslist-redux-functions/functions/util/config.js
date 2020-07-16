d66b3bb5a8dcd7024d7087d28381acef2772d17d
Andrew Park <hp1141@stern.nyu.edu>
2020-07-16 16:56:28 -0400

    Refactoring & organization

---
+++ backend/craigslist-redux-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCH4uQXn_ae2O0oUMLWI-mo0bRoJvik8RU",
+  authDomain: "craigslist-redux.firebaseapp.com",
+  databaseURL: "https://craigslist-redux.firebaseio.com",
+  projectId: "craigslist-redux",
+  storageBucket: "craigslist-redux.appspot.com",
+  messagingSenderId: "197322782010",
+  appId: "1:197322782010:web:1527d5098ca8219c454c03",
+  measurementId: "G-HRHKEQ1XSD",
+};
