ef3f5fb82f73fdd1550d4e919272a13f59174215
Andrew Park <hp1141@stern.nyu.edu>
2020-07-23 02:34:30 -0400

    Logout feature

--- frontend/src/components/nav/Navbars.js
+++ frontend/src/components/nav/Navbars.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import UserContext from "../../context/UserContext";
 
 // Import Bootstrap styles
-import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import UserLocation from "./UserLocation";
 
 const Navbars = () => {
+  const { userData, setUserData } = useContext(UserContext);
+
+  const logout = () => {
+    setUserData({
+      token: undefined,
+      user: undefined,
+    });
+    localStorage.setItem("auth-token", "");
+  };
+
   return (
     <header>
       <Navbar bg="light" variant="light">
@@ -22,12 +33,18 @@ const Navbars = () => {
           <UserLocation />
         </Nav>
         <Nav>
-          <Nav.Link as={Link} to="/login">
-            Login
-          </Nav.Link>
-          <Nav.Link as={Link} to="/signup">
-            Sign Up
-          </Nav.Link>
+          {userData.user ? (
+            <Nav.Link onClick={logout}>Log Out</Nav.Link>
+          ) : (
+            <React.Fragment>
+              <Nav.Link as={Link} to="/login">
+                Login
+              </Nav.Link>
+              <Nav.Link as={Link} to="/signup">
+                Sign Up
+              </Nav.Link>
+            </React.Fragment>
+          )}
         </Nav>
         {/* <Form inline>
           <FormControl
