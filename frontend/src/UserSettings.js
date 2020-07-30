0009d9eafa8c06ea04209b29d1c68683fe30d381
Andrew Park <hp1141@stern.nyu.edu>
2020-07-30 00:17:06 -0400

    installed multer for backend

--- frontend/src/UserSettings.js
+++ frontend/src/UserSettings.js
@@ -1,11 +1,16 @@
-import React, { useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { Container, Form, Button, Row, Col } from "react-bootstrap";
+import axios from "axios";
 import UserContext from "./context/UserContext";
 
 export default function UserSettings() {
   const { userData } = useContext(UserContext);
   const history = useHistory();
+
+  const [file, setFile] = useState("");
+  const [filename, setFilename] = useState("No File Chosen");
+  const [uploadedFile, setUploadedFile] = useState({});
 
   // Prevent editing when not logged in
   useEffect(() => {
@@ -14,7 +19,29 @@ export default function UserSettings() {
     }
   }, [userData]);
 
-  console.log(userData.user);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const res = await axios.post('http://localhost:5000/users/uploadImage', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+
+      const { fileName, filePath } = res.data;
+      
+      setUploadedFile({ fileName, filePath });
+    } catch (err) {
+      if (err.response.status === 500) {
+        console.log("There was a problem with the server")
+      } else {
+        console.log(err.response.data.msg);
+      }
+    }
+  };
 
   return (
     <Container fluid>
@@ -39,11 +66,18 @@ export default function UserSettings() {
 
             <Form.Row>
               <Form.Group>
-                <Form.File id="uploadAvatar" label="Upload avatar" />
+                <Form.File
+                  id="uploadAvatar"
+                  label="Upload avatar"
+                  onChange={(e) => {
+                    setFile(e.target.files[0]);
+                    setFilename(e.target.files[0].name);
+                  }}
+                />
               </Form.Group>
             </Form.Row>
 
-            <Button variant="dark" type="submit">
+            <Button variant="dark" type="submit" onClick={onSubmit}>
               Submit
             </Button>
           </Form>
