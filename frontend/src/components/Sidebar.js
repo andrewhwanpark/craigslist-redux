27fd4d96597e08294ab656459c89022e9ca76626
Andrew Park <hp1141@stern.nyu.edu>
2020-08-19 13:56:33 -0400

    Use shared component for sidebar location selector

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,36 +1,34 @@
 import React, { useState } from "react";
-import Select from "react-select";
-import { cities } from "../cities";
 import { Col, Row } from "react-bootstrap";
 import { isNullable } from "../utils/null-checks";
+import LocationSelector from "./shared/LocationSelector";
 
 const Sidebar = ({ setLocation }) => {
   const [displayValue, setDisplayValue] = useState([]);
 
+  const onChange = (e) => {
+    // User clears search, show all listing
+    if (isNullable(e)) {
+      setDisplayValue(null);
+      setLocation(undefined);
+      return;
+    }
+
+    setDisplayValue([
+      {
+        label: e.label,
+        value: e.value,
+      },
+    ]);
+    setLocation(e.value);
+  };
+
   return (
     <Row className="my-4">
       <Col xl={12} lg={12} md={12} sm={12} xs={12}>
-        <Select
-          isSearchable
-          isClearable
-          placeholder="Search your city"
-          onChange={(e) => {
-            // User clears search, show all listing
-            if (isNullable(e)) {
-              setDisplayValue(null);
-              setLocation(undefined);
-              return;
-            }
-
-            setDisplayValue([
-              {
-                label: e.label,
-                value: e.value,
-              },
-            ]);
-            setLocation(e.value);
-          }}
-          options={cities}
+        <LocationSelector
+          isClearable={true}
+          onChange={onChange}
           value={displayValue}
         />
       </Col>
--- frontend/src/components/shared/LocationSelector.js
+++ frontend/src/components/shared/LocationSelector.js
@@ -2,11 +2,18 @@ import React from "react";
 import Select from "react-select";
 import { cities } from "../../cities";
 
-const LocationSelector = ({ onChange, className }) => {
+const LocationSelector = ({
+  onChange,
+  className,
+  isClearable,
+  displayValue,
+}) => {
   return (
     <Select
       isSearchable
+      isClearable={isClearable}
       placeholder="Search for your city"
+      value={displayValue}
       onChange={onChange}
       options={cities}
       className={className}
