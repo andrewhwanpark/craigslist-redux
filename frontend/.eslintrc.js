34d84b520764aed5dd1db4bcb5a7ed01fdaf99a2
Andrew Park <hp1141@stern.nyu.edu>
2020-08-20 04:05:12 -0400

    Configure eslint-config-prettier

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  extends: ["plugin:react/recommended", "airbnb", "prettier", "prettier/react"],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly",
