c490afffb1f8373f68eb5882bf0a814114227afe
Andrew Park <hp1141@stern.nyu.edu>
2020-08-21 02:39:23 -0400

    eslint-plugin-node rules

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -14,5 +14,15 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 11,
   },
-  rules: {},
+  rules: {
+    "node/exports-style": ["error", "module.exports"],
+    "node/file-extension-in-import": ["error", "always"],
+    "node/prefer-global/buffer": ["error", "always"],
+    "node/prefer-global/console": ["error", "always"],
+    "node/prefer-global/process": ["error", "always"],
+    "node/prefer-global/url-search-params": ["error", "always"],
+    "node/prefer-global/url": ["error", "always"],
+    "node/prefer-promises/dns": "error",
+    "node/prefer-promises/fs": "error",
+  },
 };
