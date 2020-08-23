10a146221a1e125395516408544e04b3f706bacf
Andrew Park <hp1141@stern.nyu.edu>
2020-08-23 16:42:44 -0400

    Add link to seller page in listing user card

--- frontend/src/components/listing/ListingUserInfo.jsx
+++ frontend/src/components/listing/ListingUserInfo.jsx
@@ -1,26 +1,29 @@
 import React from "react";
-import { Container, Image } from "react-bootstrap";
+import { Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const ListingUserInfo = ({ writer }) => {
   return (
-    <Container fluid>
-      <Image
-        src={`http://localhost:5000/${writer.image.filePath}`}
-        roundedCircle
-        width="50"
-        height="50"
-        className="mr-2"
-        style={{ float: "left" }}
-      />
-      <Link to={`/${writer.username}`}>
+    <>
+      <Link
+        to={`/${writer.username}`}
+        className="link-no-style text-decoration-none font-weight-bold"
+      >
+        <Image
+          src={`http://localhost:5000/${writer.image.filePath}`}
+          roundedCircle
+          width="50"
+          height="50"
+          className="mr-2"
+          style={{ float: "left" }}
+        />
         <span>
           {writer.username} ({writer.sold})
         </span>
       </Link>
       <br />
       <span>0 listings for sale</span>
-    </Container>
+    </>
   );
 };
 
