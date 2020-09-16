e6a85ee8585072c6b90fef9fe4f3d1bc58f83dc5
Andrew Park <hp1141@stern.nyu.edu>
2020-09-15 21:47:28 -0400

    Change signup placeholders

--- client/src/components/auth/Signup.jsx
+++ client/src/components/auth/Signup.jsx
@@ -81,7 +81,7 @@ const Signup = () => {
           <Form.Label>Email address</Form.Label>
           <Form.Control
             type="email"
-            placeholder="Enter email"
+            placeholder="You can use a fake email"
             onChange={(e) => {
               setEmail(e.target.value);
             }}
@@ -91,7 +91,7 @@ const Signup = () => {
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
-            placeholder="Enter Password"
+            placeholder="Do not use sensitive password"
             onChange={(e) => {
               setPassword(e.target.value);
             }}
